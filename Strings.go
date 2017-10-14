53816649a3e703017ce3981d3be1eefb7d803ca6
Keloc13 <kvnyn10@gmail.com>
2017-10-13 18:52:25 -0700

    Test3

---
+++ Strings.go
@@ -0,0 +1,56 @@
+package main 
+
+import ("fmt" 
+"strings"
+"sort"
+"os"
+"log"
+"io/ioutil")
+
+func main(){
+	samString := "Hello world";
+
+	fmt.Println(strings.Contains(samString, "lo"))
+	fmt.Println(strings.Index(samString, "lo"))
+	fmt.Println(strings.Count(samString, "l"))
+
+	testString1();
+	createFile();
+}
+
+func testString1(){
+
+	csvString := "1,2,3,4,5,6";
+
+	fmt.Println(strings.Split(csvString,","));
+
+	listOfLetters := []string{"c","a","b"};
+
+	sort.Strings(listOfLetters);
+
+	fmt.Println("Letters: ", listOfLetters);
+}
+
+func createFile(){
+
+	if _, er := os.Stat("sample.txt");
+	os.IsNotExist(er){
+		file, err := os.Create("samp.txt");
+
+		if err != nil{
+		log.Fatal(err);
+	}
+
+	file.WriteString("This is some random text");
+	file.Close();
+	}
+	
+	stream, err := ioutil.ReadFile("samp.txt");
+
+	if err != nil{
+		log.Fatal(err);
+	}
+
+	readString := string(stream);
+	fmt.Println(readString);
+}
